1ab51572296c61544443670f4b03d7eb2bf11bae
Rob Quatela <Rob Quatela@RQDEVELOPER.mshome.net>
2018-02-26 09:58:19 -0500

    troubleshooting tomcat with spring-mvc dependencies

---
+++ repairity/src/main/java/com/accountomation/repairity/controller/IncidentController.java
@@ -0,0 +1,10 @@
+package com.accountomation.repairity.controller;
+
+import org.springframework.stereotype.Controller;
+
+public class IncidentController {
+
+	public String showHomePage() {
+		return "index";
+	}
+}
